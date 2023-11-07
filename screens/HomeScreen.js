226577b56241fe0fc7efd2ceb2041be1ec985641
junrongzhu1 <marcelo.sv_graviti@hotmail.com>
2023-11-07 20:14:49 -0300

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,41 @@
+import { View, Text, TextInput} from 'react-native'
+import React, { Component } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { StatusBar } from 'expo-status-bar'
+import * as Icon from "react-native-feather"
+import { themeColors } from '../theme'
+import { ScrollView } from 'react-native'
+import Categories from '../componentes/categories'
+
+
+export default function HomeScreen(){
+  return(
+    <SafeAreaView className="bg-white">
+      <StatusBar barStyle="dark-content"/>
+      {/* search bar*/}
+      <View className="flex-row items-center space-x-2 px-4 pb-2">
+        <View className="flex-row flex-1 items-center p-3 rounded-full border border-gray-3">
+          <Icon.Search height="25" width="25" stroke="gray"/>
+          <TextInput placeholder='Restaurants' className="ml-2 flex-1"/>
+          <View className="flex-row items-center space-x-1 border-0 border-1-2 pl-2 border-gray-3">
+              <Icon.MapPin height="20" width="20" stroke="gray" />
+              <Text className="text-gray-600">Belford Roxo, RJ</Text>
+          </View>
+        </View>  
+        <View style={{backgroundColor: themeColors.bgColor(1)}} className="p-3 rounded-full">
+          <Icon.Sliders height="20" width="20" strokeWidth={2.5} stroke="white"/>
+        </View>
+      </View>
+
+      {/* main */}
+      <ScrollView showsVerticalScrollIndicatior={false}
+        contentContainerStyle={{
+          paddingBottom: 20
+        }}
+      >
+        {/*Categories*/}
+        <Categories />
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
